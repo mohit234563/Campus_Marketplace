08efda091bac2056ca8234bb6c22729cf0ca4d1c
mohit234563 <mohitvj721@gmail.com>
2026-07-28 22:53:58 +0530

    rename issues

--- Frontend/src/pages/publicProfilePage.jsx
+++ Frontend/src/pages/publicProfilePage.jsx
@@ -32,6 +32,7 @@ const PublicprofilePage = () => {
             }
         };
         fetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [username]);
 
     if (loading) return (
